3a414f216db975ce78e5739c6059fc423b191cc5
sadykova <almira.sadyk@gmail.com>
2022-04-12 09:23:44 -0400

    Reverse a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    return string.repeat(num);
+  }
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let newString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
 };
+
+console.log(reverseString("hello"));
+console.log(reverseString("it is gonna be Okay"));
+console.log(reverseString(""));
 
 // Do not edit below this line
 module.exports = reverseString;
