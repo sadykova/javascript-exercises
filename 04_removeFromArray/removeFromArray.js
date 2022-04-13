af6dbcd64132ef2286bca4300a0b22e11cf085a2
sadykova <almira.sadyk@gmail.com>
2022-04-13 05:56:21 -0400

    Write removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, element) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == element) {
+      array.splice(i, 1);
+      return array;
+    }
+  }
 };
 
-// Do not edit below this line
-module.exports = removeFromArray;
+console.log(removeFromArray([1, 2, 3, 4], 4));
+
+// let array = [1, 2, 3, 4];
+// let newArray = [];
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] === 4) {
+//     newArray = array.splice(i, 1);
+//   }
+// }
+// console.log(newArray);
+// console.log(array);
+
+// // Do not edit below this line
+// module.exports = removeFromArray;
