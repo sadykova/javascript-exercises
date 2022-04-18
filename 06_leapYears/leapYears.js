dca6972c205d9d1cf1e211d201127e3f664ea15b
sadykova <almira.sadyk@gmail.com>
2022-04-18 13:40:08 -0400

    Determine if a year is leap year or not

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 400 == 0) {
+    return true;
+  } else if (year % 100 == 0) {
+    return false;
+  } else if (year % 4 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(leapYears(700));
+console.log(leapYears(1996));
+console.log(leapYears(1997));
+console.log(leapYears(34992));
+console.log(leapYears(1900));
+console.log(leapYears(1600));
 
 // Do not edit below this line
 module.exports = leapYears;
