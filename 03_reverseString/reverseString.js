09e282259617aa3af36280c81e34b5655f1c6b95
sadykova <almira.sadyk@gmail.com>
2022-04-18 14:10:26 -0400

    Reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -6,9 +6,6 @@ const reverseString = function (string) {
   return newString;
 };
 
-console.log(reverseString("hello"));
-console.log(reverseString("it is gonna be Okay"));
-console.log(reverseString(""));
 
 // Do not edit below this line
 module.exports = reverseString;
