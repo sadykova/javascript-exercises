ad1067b1252c9083551a26461a110d5925b5b95a
sadykova <almira.sadyk@gmail.com>
2022-04-18 14:09:04 -0400

    Converts temperature from C to F and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let celsius = (temp - 32) * (5 / 9);
+  return Math.round(celsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let f = temp * (9 / 5) + 32;
+  return Math.round(f * 10) / 10;
 };
+
+console.log(ftoc(100));
+console.log(ftoc(-100));
+console.log(ctof(0));
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
